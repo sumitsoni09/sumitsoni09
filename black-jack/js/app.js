33192a4ca3fcce95e6364f0bf455b76094f389c7
sumitsoni09 <41563314+sumitsoni09@users.noreply.github.com>
2018-09-26 19:27:24 -0500

    buttons working

--- black-jack/js/app.js
+++ black-jack/js/app.js
@@ -2,17 +2,23 @@
   //bid button should be able to add money to the total after a win/loss of deal also starts the game
   //bid text should be disabled during play  //hit button sends one card to the player
   //stay button stops the players turn
-  //win/lose by who "busts" and stays up to 21 or under
-  //assign each card a value, A=1, 2=2, 3=3,..., J=11, Q=12, K=13
+  //win\lose by who "busts" and stays up to 21 or under
+  //assign each card a value, A=1, 2=2, 3=3,..., J=10, Q=10, K=10
   //to win a game must reach 21
   //prompt for how to play
-  let value = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K',]
-  let suit = ['Clubs', 'Diamonds', 'Spades', 'Hearts']
+  let value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  let suit = ['Clubs', 'Diamonds', 'Spades', 'Hearts'];
+  let deck = new Array ();
+
 class Deck {
   constructor() {
     this.deck=[]
     this.dealt_cards=[]
 
+
+  }
+  singlecard() {
+    return this.deck[0]
   }
   generateDeck() {
     // let card = (suit, value) => {
@@ -51,3 +57,29 @@ deck = new Deck()
 console.log(deck);
 deck.generateDeck()
 deck.printDeck()
+
+//---------- game logic------------
+//player has $100
+//player must make a bid to start game
+//if player gets 21 points/blackjack on first deal player stays auto then Dealer starts, else player has to decide if they want to hit or stay to reach 21 then dealer starts when player stays
+//if player is over 21 he busts/loses money and if dealer busts player wins bet
+//
+$(()=> {
+  const $start = ()=>{
+  }
+  const $hit = ()=>{
+    console.log("Hit");
+  }
+
+  const $stay = ()=>{
+    console.log("Stay");
+  }
+
+  const $bid = ()=>{
+    console.log("Bid");
+  }
+
+  $("#bid-btn").on('click', $bid);
+  $("#stay-btn").on('click', $stay);
+  $("#hit-btn").on('click', $hit);
+});
