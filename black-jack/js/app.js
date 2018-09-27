0807362706df88b35ebfeb537b5a087cb95ef81a
sumitsoni09 <41563314+sumitsoni09@users.noreply.github.com>
2018-09-27 05:07:17 -0500

    added style and bid button to work

--- black-jack/js/app.js
+++ black-jack/js/app.js
@@ -7,8 +7,7 @@
   //to win a game must reach 21
   //prompt for how to play
   let value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-  let suit = ['Clubs', 'Diamonds', 'Spades', 'Hearts'];
-  let deck = new Array ();
+  let suit = ['♦','♣','♥','♠']
 
 class Deck {
   constructor() {
@@ -21,12 +20,7 @@ class Deck {
     return this.deck[0]
   }
   generateDeck() {
-    // let card = (suit, value) => {
-    //   this.name = value + ' of ' + suit
-    //   this.suit = suit
-    //   this.value = value
-    //
-    // }
+
 
     for (let s = 0; s < suit.length; s++) {
       for (let v = 0; v < value.length; v++) {
@@ -35,11 +29,10 @@ class Deck {
           this.suit = suit
           this.value = value
           return {name: this.name, suit: this.suit, value: this.value}
-
         }
-        this.deck.push(card(suit[s], value[v]))
-        console.log(suit[s])
-        console.log(value[v])
+      this.deck.push(card(suit[s], value[v]))
+        console.log(suit[s]);
+        console.log(value[v]);
       }
     }
   }
@@ -48,15 +41,19 @@ class Deck {
       console.log('the deck has been generated');
     } else {
       for (let c = 0; c < this.deck.length; c++) {
-        console.log(this.deck[c]);
+        // console.log(this.deck[c]);
       }
     }
   }
-}
-deck = new Deck()
+
+
+deck = new Deck();
 console.log(deck);
-deck.generateDeck()
-deck.printDeck()
+deck.generateDeck();
+deck.printDeck();
+
+
+
 
 //---------- game logic------------
 //player has $100
@@ -64,22 +61,32 @@ deck.printDeck()
 //if player gets 21 points/blackjack on first deal player stays auto then Dealer starts, else player has to decide if they want to hit or stay to reach 21 then dealer starts when player stays
 //if player is over 21 he busts/loses money and if dealer busts player wins bet
 //
+function getAccount()
+{
+  var bidBank= $("#bid-box").val();
+  $("#bank").html(bidBank);
+}
+
 $(()=> {
+
   const $start = ()=>{
+
   }
   const $hit = ()=>{
+
     console.log("Hit");
   }
-
   const $stay = ()=>{
     console.log("Stay");
   }
-
   const $bid = ()=>{
-    console.log("Bid");
-  }
 
+    $('#bid-box').val('');
+
+  }
   $("#bid-btn").on('click', $bid);
   $("#stay-btn").on('click', $stay);
   $("#hit-btn").on('click', $hit);
+
+
 });
